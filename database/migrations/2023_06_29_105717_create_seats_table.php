2f559dd3b3c58a3edb2ce29a6b6025b10f855608
Farzad Foroozanfar <foruozanfar2000f@gmail.com>
2023-06-29 11:43:23 +0330

    create all migrations

---
+++ database/migrations/2023_06_29_105717_create_seats_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('seats', function (Blueprint $table) {
+            $table->engine = "InnoDB";
+            $table->id();
+            $table->integer('row');
+            $table->integer('col');
+            $table->unsignedBigInteger('sans_id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->foreign('sans_id')
+                ->references('id')->on('sans')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('seats');
+    }
+};
