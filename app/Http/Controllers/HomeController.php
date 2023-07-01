8e0485c91a8458c2b8bed74536cf95cbb355bc77
Ali Attar <m.aliattarr@gmail.com>
2023-07-01 16:37:51 +0330

    create navbar

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,16 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\City;
 use App\Models\Movie;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
-    public function Index(Request $request){
+    public function index(Request $request)
+    {
         $last_movies = Movie::with('category')->latest()->take(4)->get();
-        return view('.user.home', [
-            'lastMovies' => $last_movies
+        $cities = City::get();
+
+        return view('user.home', [
+            'lastMovies' => $last_movies ,
+            'cities' => $cities,
         ]);
     }
 }
