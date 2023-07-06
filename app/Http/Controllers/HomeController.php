b93c468f3122dd6f63809d2f5c98f1bc06230b28
Farzad Foroozanfar <foruozanfar2000f@gmail.com>
2023-07-06 18:30:03 +0330

    add top movies 2 view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,22 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
 use App\Models\City;
 use App\Models\Movie;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function index(Request $request)
     {
-        $last_movies = Movie::with('category')->latest()->take(4)->get();
+        $last_movies = Movie::with('category')->latest()->take(8)->get();
+        $top_movies  = Movie::orderByDesc('sale')->take(5)->get();
+
         $cities = City::get();
 
         return view('user.home', [
             'lastMovies' => $last_movies ,
             'cities' => $cities,
+            'topMovies' => $top_movies
         ]);
     }
 }
