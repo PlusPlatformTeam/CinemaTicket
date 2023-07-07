17ab39b4d05d4f90c8edc4fd4bf3fb87ff00da61
Farzad Foroozanfar <foruozanfar2000f@gmail.com>
2023-07-07 19:39:31 +0330

    create cinema ctrl

--- app/Http/Controllers/CinemaController.php
+++ app/Http/Controllers/CinemaController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cinema;
+use App\Models\City;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class CinemaController extends Controller
 {
-    public function Index(Request $request)
+    public function Index()
     {
-        dd($request->toArray());
+        $cinemas = Cinema::with('options')->get()->toArray();
+        return view('user.cinemas', [
+            'cinemas' => $cinemas,
+            'cities' => City::all(),
+            'lastMovies' => Movie::with('category')->latest()->take(8)->get(),
+            'topMovies' => Movie::orderByDesc('sale')->take(5)->get()
+        ]);
     }
+
 }
