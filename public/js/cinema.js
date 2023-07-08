fdadfd67261189a6f7fddc7f4c68bc5452e83b0e
Farzad Foroozanfar <foruozanfar2000f@gmail.com>
2023-07-08 21:40:42 +0330

    options css & on click filter

--- public/js/cinema.js
+++ public/js/cinema.js
@@ -1,3 +1,11 @@
-$(document).ready(() => {
-    
+const optionFilter = $('#option-filter');
+const deleteIcon = optionFilter.find('.fa-sort-down');
+
+optionFilter.on('click', () => {
+    optionFilter.toggleClass('active');
+    deleteIcon.toggleClass('fa-sort-down pb-2 pt-2 fa-sort-up');
+    $('#options-container').toggle();
+    if (deleteIcon.hasClass('fa-sort-up')) {
+        $('#options-container').css('display', 'flex');
+    }
 });
